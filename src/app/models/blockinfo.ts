29eab836a7d11992d481d28d0faad286c999c1a3
Yanpei Li <yanpei.li@telus.com>
2018-05-30 11:36:35 +0800

    Updated ts types for transaction Vin

--- src/app/models/blockinfo.ts
+++ src/app/models/blockinfo.ts
@@ -1,65 +1,61 @@
-export interface blockInfo {
-
-    hash : string,
-    confirmations : number,
-    strippedsize : number,
-    size : number,
-    weight : number,
-    height : number,
-    version : number,
-    versionHex : string,
-    merkleroot : string,
-    tx : Tx[],
-    time : number,
-    date : Date,
-    mediantime : number,
-    nonceUint32 : number,
-    nonce : string,
-    bits : string,
-    difficulty : number,
-    chainwork : string,
-    previousblockhash : string,
-    nextblockhash : string;
-
+export interface BlockInfo {
+  hash: string;
+  confirmations: number;
+  strippedsize: number;
+  size: number;
+  weight: number;
+  height: number;
+  version: number;
+  versionHex: string;
+  merkleroot: string;
+  tx: Tx[];
+  time: number;
+  date: Date;
+  mediantime: number;
+  nonceUint32: number;
+  nonce: string;
+  bits: string;
+  difficulty: number;
+  chainwork: string;
+  previousblockhash: string;
+  nextblockhash: string;
 }
 
 export interface Tx {
-
-    txid : string,
-    hash  : string,
-    version :number,
-    size : number,
-    vsize : number,
-    locktime : number,
-    vin : TxVin[],
-    vout : TxVout[],
-    blockhash : string,
-    hex : string;
-
-
+  txid: string;
+  hash: string;
+  version: number;
+  size: number;
+  vsize: number;
+  locktime: number;
+  vin: TxVin[];
+  vout: TxVout[];
+  blockhash: string;
+  hex: string;
 }
 
 export interface TxVin {
-
-    coinbase : string,
-    sequence : number;
-
+  coinbase: string | null;
+  sequence: number;
+  vout: number | null;
+  scriptSig: ScriptSignature | null;
 }
 
+export interface ScriptSignature {
+  asm: string;
+  hex: string;
+}
 
 export interface TxVout {
-
-    value : number,
-    n : number,
-    scriptPubKey : ScriptPubKey;
-
+  value: number;
+  n: number;
+  scriptPubKey: ScriptPubKey;
 }
 
 export interface ScriptPubKey {
-    
-    asm : string,
-    hex : string,
-    reqSigs? : number,
-    type : string,
-    addresses : string[];
+  asm: string;
+  hex: string;
+  reqSigs: number;
+  type: string;
+  addresses: string[];
 }
--- src/app/models/chainInfo.ts
+++ src/app/models/chainInfo.ts
@@ -1,38 +1,37 @@
-export interface chainInfo {
-
-    chain : string,
-    blocks : number;
-    headers : number;
-    bestblockhash : string,
-    difficulty : number,
-    mediantime : number,
-    verificationprogress : number,
-    chainwork : string,
-    pruned : boolean,
-    softforks : sForks[],
-    bip9_softforks : bip9Forks[],
-    tmp:any; //to hold any intermediate value
+export interface ChainInfo {
+  chain: string;
+  blocks: number;
+  headers: number;
+  bestblockhash: string;
+  difficulty: number;
+  mediantime: number;
+  verificationprogress: number;
+  chainwork: string;
+  pruned: boolean;
+  softforks: SForks[];
+  bip9_softforks: Bip9Forks[];
+  tmp: any; // to hold any intermediate value
 }
 
-export interface bip9Forks {
-    csv : bForks,
-    segwit : bForks;
+export interface Bip9Forks {
+  csv: BForks;
+  segwit: BForks;
 }
 
 
-export interface bForks {
-    status : string,
-    startTime : number,
-    timeout :number,
-    since : number;
+export interface BForks {
+  status: string;
+  startTime: number;
+  timeout: number;
+  since: number;
 }
 
-export interface sForks{
-    id : string,
-    version : number,
-    reject : Status;
+export interface SForks {
+  id: string;
+  version: number;
+  reject: Status;
 }
 
-export interface Status{
-    status : boolean;
+export interface Status {
+  status: boolean;
 }
