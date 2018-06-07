e62500f56477b6b68e862746a6a775cf0f51d610
azraeljack <4312182lyp@gmail.com>
2018-06-07 10:27:44 -0400

    Update config

--- src/environments/environment.main.ts
+++ src/environments/environment.main.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   restAPIBase: 'http://18.130.8.117:8667/rest',
-  fabAPIBase: 'http://fabtest.info:9001/fabapi'
+  fabAPIBase: 'http://fabexplorer.info:9001/fabapi'
 };
