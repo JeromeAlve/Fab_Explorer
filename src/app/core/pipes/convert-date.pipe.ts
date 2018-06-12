cfbb3517cfb290f67bccdb91f981972596f385d5
azraeljack <4312182lyp@gmail.com>
2018-06-11 22:27:29 -0400

    Fix date converter pipe typo

--- src/app/core/pipes/convert-date.pipe.ts
+++ src/app/core/pipes/convert-date.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertDatePipe implements PipeTransform {
 
-  transform(value: string, args?: any): Date {
-    return !!value ? new Date(value * 1000) : null;
+  transform(value: string, args?: any): number {
+    return !!value ? Number(value) * 1000 : null;
   }
 
 }
