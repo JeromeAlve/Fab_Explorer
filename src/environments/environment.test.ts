5ae8843daea8e6ef5692fd1fc1817f7888a8142f
azraeljack <4312182lyp@gmail.com>
2018-06-20 15:32:14 -0400

    Fix config typo in test net

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   updateFreq: 35,
   restAPIBase: 'http://fabtest.info:18667/rest',
-  utxoAPIBase: 'http://fabexplorer.info:8666',
+  utxoAPIBase: 'http://fabtest.info:8666',
   fabAPIBase: 'http://fabtest.info:9001/fabapi',
   cacheExpireTime: 60 * 60 * 24,
   autoExpireSearch: 60 * 2
